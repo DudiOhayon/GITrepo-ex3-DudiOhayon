b5fe42f5652750823e30fdb13eee213d28d58a87
Your Name <you@example.com>
2024-12-11 21:53:46 +0200

    Changing the print

--- DudiOhayon.java
+++ DudiOhayon.java
@@ -1,7 +1,7 @@
 public class DudiOhayon {
     public static void main(String[] args) {
         for (int i = 1; i <= 10; i++) {
-            System.out.println(i*i+1);
+            System.out.println(i*i);
         }
     }
 }
